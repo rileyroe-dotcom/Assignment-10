732de221cb2994da8998a1475ecbde1053c94967
rileyroe-dotcom <riley.roe@coyotes.usd.edu>
2026-04-27 18:28:05 -0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,72 @@
+const display = document.getElementById("timeDisplay") as HTMLElement;
+const startButton = document.getElementById("startBtn") as HTMLButtonElement;
+const stopButton = document.getElementById("stopBtn") as HTMLButtonElement;
+const clearButton = document.getElementById("clearBtn") as HTMLButtonElement;
+const lapButton = document.getElementById("lapBtn") as HTMLButtonElement;
+const clearLapsButton = document.getElementById("clearLapsBtn") as HTMLButtonElement;
+const laps = document.getElementById("lapsList") as HTMLUListElement;
+
+let totalTime = 0;
+let running = false;
+let interval = 0;
+
+function showTime(): void {
+    const minutes = Math.floor(totalTime / 60000);
+    const seconds = Math.floor((totalTime % 60000) / 1000);
+    const hundredths = Math.floor((totalTime % 1000) / 10);
+
+    let minuteText = minutes.toString();
+    let secondText = seconds.toString();
+    let hundredthsText = hundredths.toString();
+
+    if (minutes < 10) {
+        minuteText = "0" + minuteText;
+    }
+
+    if (seconds < 10) {
+        secondText = "0" + secondText;
+    }
+
+    if (hundredths < 10) {
+        hundredthsText = "0" + hundredthsText;
+    }
+
+    display.textContent = minuteText + ":" + secondText + "." + hundredthsText;
+}
+
+startButton.onclick = function () {
+    if (!running) {
+        running = true;
+
+        interval = setInterval(function () {
+            totalTime += 10;
+            showTime();
+        }, 10);
+    }
+};
+
+stopButton.onclick = function () {
+    if (running) {
+        clearInterval(interval);
+        running = false;
+    }
+};
+
+clearButton.onclick = function () {
+    clearInterval(interval);
+    running = false;
+    totalTime = 0;
+    showTime();
+};
+
+lapButton.onclick = function () {
+    const lap = document.createElement("li");
+    lap.textContent = display.textContent;
+    laps.appendChild(lap);
+};
+
+clearLapsButton.onclick = function () {
+    laps.innerHTML = "";
+};
+
+showTime();
